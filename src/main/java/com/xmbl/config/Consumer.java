7dcaf9f0c1aa70ad579934525f6dca5ca66d61ef
Noseparte <noseparte@aliyun.com>
2018-08-28 21:35:41 +0800

    jms
    高可用消息队列ActiveMQ在Springboot中的用武之地

--- src/main/java/com/xmbl/config/Consumer.java
+++ src/main/java/com/xmbl/config/Consumer.java
@@ -4,10 +4,14 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.xmbl.util.email.EmailUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
+import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
 import javax.jms.Session;
 import javax.jms.TextMessage;
@@ -22,25 +26,25 @@ import javax.jms.TextMessage;
  */
 @Slf4j
 @Component
-public class Consumer {
+    public class Consumer {
 
-    @JmsListener(destination = "emailQueue")
-    public void receiveEmailQueue(final TextMessage text, Session session)
-            throws JMSException {
-        try {
-            log.info("邮件队列收到的报文为: message,{}", text.getText());
-            text.acknowledge();// 使用手动签收模式，需要手动的调用，如果不在catch中调用session.recover()消息只会在重启服务后重发
-            JSONObject jsonObj = JSON.parseObject(text.getText());
-            boolean isSucc = EmailUtils.sendMailForSystem(jsonObj);
-            Assert.isTrue(isSucc, "发送邮件失败");
-        } catch (Exception e) {
-            log.error("errorMsg:{ ============================ 发送邮件失败,错误信息为：,errorMsg,{}", e.getMessage());
-            session.recover();// 此不可省略 重发信息使用
+        @JmsListener(destination = "emailQueue")
+        public void receiveEmailQueue(final TextMessage text, Session session)
+                throws JMSException {
+            try {
+                log.info("邮件队列收到的报文为: message,{}", text.getText());
+                text.acknowledge();// 使用手动签收模式，需要手动的调用，如果不在catch中调用session.recover()消息只会在重启服务后重发
+                JSONObject jsonObj = JSON.parseObject(text.getText());
+                boolean isSucc = EmailUtils.sendMailForSystem(jsonObj);
+                Assert.isTrue(isSucc, "发送邮件失败");
+            } catch (Exception e) {
+                log.error("errorMsg:{ ============================ 发送邮件失败,错误信息为：,errorMsg,{}", e.getMessage());
+                session.recover();// 此不可省略 重发信息使用
+            }
         }
-    }
 
 
-    @JmsListener(destination = "smsQueue")
+        @JmsListener(destination = "smsQueue")
     public void receiveSMSQueue(final TextMessage text, Session session)
             throws JMSException {
         try {
@@ -54,5 +58,29 @@ public class Consumer {
             session.recover();// 此不可省略 重发信息使用
         }
     }
+
+    @JmsListener(destination = "emailTopic",containerFactory = "myJmsContainerFactory")
+    public void receiveEmailTopic(final TextMessage text, Session session)
+            throws JMSException {
+        try {
+            log.info("邮件主题收到的报文为: message,{}", text.getText());
+            text.acknowledge();// 使用手动签收模式，需要手动的调用，如果不在catch中调用session.recover()消息只会在重启服务后重发
+            JSONObject jsonObj = JSON.parseObject(text.getText());
+            boolean isSucc = EmailUtils.sendMailForSystem(jsonObj);
+            Assert.isTrue(isSucc, "发送邮件失败");
+        } catch (Exception e) {
+            log.error("errorMsg:{ ============================ 发送邮件失败,错误信息为：,errorMsg,{}", e.getMessage());
+            session.recover();// 此不可省略 重发信息使用
+        }
+    }
+
+    @Bean
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory){
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setPubSubDomain(true);
+        return factory;
+    }
+
 
 }
--- src/main/java/com/xmbl/controller/jms/PTJmsController.java
+++ src/main/java/com/xmbl/controller/jms/PTJmsController.java
@@ -9,6 +9,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,19 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.jms.Destination;
 
-
 /**
  * Copyright © 2018 noseparte © BeiJing BoLuo Network Technology Co. Ltd.
  *
  * @Author Noseparte
  * @Compile 2018-08-14 -- 16:34
  * @Version 1.0
- * @Description     ActiveMQ 消息队列管理中心i
+ * @Description ActiveMQ 消息队列管理中心i
  */
 @Slf4j
 @RestController
 @RequestMapping(value = Route.PATH + Route.Jms.PATH)
-@Api(value = "消息队列",description = "ActiveMQ 消息队列")
+@Api(value = "消息队列", description = "ActiveMQ 消息队列")
 public class PTJmsController extends BaseController {
 
     @Autowired
@@ -42,29 +42,55 @@ public class PTJmsController extends BaseController {
      * @return
      */
     @ApiOperation(value = "添加邮件任务到消息队列")
-    @ApiImplicitParam(name = "消息内容",value = "jsonData",required = true,dataType = "String")
-    @RequestMapping(value = Route.Jms.SEND_EMAIL_MESSAGE,method = RequestMethod.POST)
-    public Response sendEmailQueue(@RequestParam(value = "jsonData",required = true) String jsonData){
+    @ApiImplicitParam(name = "消息内容", value = "jsonData", required = true, dataType = "String")
+    @RequestMapping(value = Route.Jms.SEND_EMAIL_MESSAGE, method = RequestMethod.POST)
+    public Response sendEmailQueue(@RequestParam(value = "jsonData", required = true) String jsonData) {
         long startTime = System.currentTimeMillis();
-        log.info("infoMsg: ================================= 添加邮件任务到消息队列开始,startTime,{}",startTime);
+        log.info("infoMsg: ================================= 添加邮件任务到消息队列开始,startTime,{}", startTime);
         Response reponse = this.getReponse();
-        try{
+        try {
             Destination emailDestination = new ActiveMQQueue("emailQueue");
-            producer.sendMessage(emailDestination,jsonData);
+            producer.sendMessage(emailDestination, jsonData);
 
             long endTime = System.currentTimeMillis();
-            log.info("infoMsg: ================================= 添加邮件任务到消息队列结束,endTime,{}",endTime);
+            log.info("infoMsg: ================================= 添加邮件任务到消息队列结束,endTime,{}", endTime);
             long spendTIme = endTime - startTime;
-            log.info("infoMsg: ================================= 共计消耗时间,spendTIme,{}",spendTIme);
+            log.info("infoMsg: ================================= 共计消耗时间,spendTIme,{}", spendTIme);
             return reponse.success();
-        }catch (Exception e){
-            log.error("errorMsg:{ ============================== 添加邮件任务到消息队列失败,errorMessage,{}",e.getMessage());
+        } catch (Exception e) {
+            log.error("errorMsg:{ ============================== 添加邮件任务到消息队列失败,errorMessage,{}", e.getMessage());
             return reponse.failure(e.getMessage());
         }
     }
 
 
+    /**
+     * 添加邮件任务到订阅队列
+     *
+     * @param jsonData
+     * @return
+     */
+    @ApiOperation(value = "添加邮件任务到订阅队列")
+    @ApiImplicitParam(name = "消息内容", value = "jsonData", required = true, dataType = "String")
+    @RequestMapping(value = Route.Jms.SUBSCRIBE_EMAIL_MESSAGE, method = RequestMethod.POST)
+    public Response sendEmailTopic(@RequestParam(value = "jsonData", required = true) String jsonData) {
+        long startTime = System.currentTimeMillis();
+        log.info("infoMsg: ================================= 添加邮件任务到订阅队列开始,startTime,{}", startTime);
+        Response reponse = this.getReponse();
+        try {
+            Destination emailDestination = new ActiveMQTopic("emailTopic");
+            producer.sendMessage(emailDestination, jsonData);
 
+            long endTime = System.currentTimeMillis();
+            log.info("infoMsg: ================================= 添加邮件任务到订阅队列结束,endTime,{}", endTime);
+            long spendTIme = endTime - startTime;
+            log.info("infoMsg: ================================= 共计消耗时间,spendTIme,{}", spendTIme);
+            return reponse.success();
+        } catch (Exception e) {
+            log.error("errorMsg:{ ============================== 添加邮件任务到订阅队列失败,errorMessage,{}", e.getMessage());
+            return reponse.failure(e.getMessage());
+        }
+    }
 
 
 }
